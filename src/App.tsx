9d1c3b98ae15414092a67c228bba2a3515e71baf
Asgar <mdnadeem_asgar@optum.com@LAMU0DW6X1WG7J7.uhc.com>
2025-06-20 01:04:29 +0530

    some changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import FormInput from "./components/FormInput";
